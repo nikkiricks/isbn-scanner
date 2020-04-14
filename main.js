f8d54bf6ebb159463affb07cef3ac4bceda29fb5
nikkiricks <nikki.ricks@gmail.com>
2020-04-14 13:13:53 +1000

    working with goodreads api

---
+++ main.js
@@ -0,0 +1,18 @@
+var request = require('request-promise');
+
+require('dotenv').config();
+
+let options = {
+    method: 'GET',
+    uri: `https://www.goodreads.com/search/index.xml?${process.env.key}&q=9788877547224`,
+    qs: {
+        key: process.env.KEY,
+        v: process.env.VERSION,
+        shelf: process.env.SHELF,
+        per_page: process.env.PER_PAGE
+    }
+}
+
+request(options).then((shelf) => {
+    console.log(shelf);
+}).catch(err => console.error(err));
